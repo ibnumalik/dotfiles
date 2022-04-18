88c2fd3bc2f027b8b11ca16fc398ba35f457a64c
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-04-18 20:35:58 +0800

    feat(wezterm): reduce opacity and suppress glyph warning

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,5 +5,6 @@ return {
     font = wezterm.font_with_fallback({"JetBrains Mono NL", "Fira Code"}),
     line_height = 1.4,
     hide_tab_bar_if_only_one_tab = true,
-    window_background_opacity = 1.0
+    window_background_opacity = 0.99,
+    warn_about_missing_glyphs = false
 }
